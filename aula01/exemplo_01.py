4e42d3c0078b97da980420b4c070d5fbb921ea68
Felipe Francisco <fsfrancisco@gmail.com>
2024-03-29 23:05:31 -0300

    feat: aula 01 - primeiro print com input

---
+++ aula01/exemplo_01.py
@@ -0,0 +1,7 @@
+# Crie um programa que o usuário digita o seu nome e retorna o número de caracteres:
+
+nome = input("Digite o seu nome: ")
+
+qtd_caracteres = len(nome)
+
+print(f'Olá {nome}, o número de caracteres do seu nome é {qtd_caracteres}')
