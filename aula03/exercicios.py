f2818bc22283541ac079ef46e045f120b9610566
Felipe Francisco <fsfrancisco@gmail.com>
2024-03-30 17:25:01 -0300

    refact: inclusao de comentario

--- aula03/exercicios.py
+++ aula03/exercicios.py
@@ -8,6 +8,9 @@
 # Imagine que você está trabalhando com dados de sensores IoT. 
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
 # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# Temperatura < 18°C é 'Baixa'
+# Temperatura >= 18°C e <= 26°C é 'Normal'
+# Temperatura > 26°C é 'Alta'
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
