623edca6996cc6d759300041bd9535b60578df9a
Felipe Francisco <fsfrancisco@gmail.com>
2024-03-31 01:46:56 -0300

    feat: adicionando relação de exercicios aula 04

---
+++ aula04/exercicios.py
@@ -0,0 +1,17 @@
+# Exercícios de Listas e Dicionários
+
+# 1. Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+# 2. Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+# 2. Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+# 4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+# 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+# 6. Dada uma lista de emails, remover todos os duplicados.
+# 7. Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+# 8. Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
+# 9. Dado um conjunto de números, calcular a média.
+# 10. Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
+# 11. Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+# 12. Dados dois dicionários, fundi-los em um único dicionário.
+# 13. Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0
+# 14. Dado um dicionário, criar listas separadas para suas chaves e valores.
+# 15. Dada uma string, contar a frequência de cada caractere usando um dicionário.
