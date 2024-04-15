adb73e0967972f49cfaeb18641f710cdd64e01ac
Felipe Francisco <fsfrancisco@gmail.com>
2024-04-15 11:37:07 -0300

    feat: exercicio 01 media de listas

--- aula07/exercicios/exercicios_01_media_lista.py
+++ aula07/exercicios/exercicios_01_media_lista.py
@@ -8,6 +8,23 @@ lista = [1,1,1,2,2,2]
 soma = 0
 for i in lista:
     soma += i
+    tamanho = len(lista)
     
-print(soma)
+print(soma, tamanho, soma/tamanho)
+# %%
+def media_valores_lista(lista: list) -> int:
+    '''
+    Calcula a media dos valores de uma lista
+    '''
+    soma = 0
+    for i in lista:
+        soma += i
+    tamanho = len(lista)
+    media = soma / tamanho
+    return print(f'Tamanho: {tamanho}, soma: {soma} e média: {media}')
+# %%
+lista = [1,2]
+
+retorno = media_valores_lista(lista)
+retorno
 # %%
